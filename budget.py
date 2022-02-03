598479f5e4e0473c08025fdc1ddfa5db20b6b740
HEATHERG LEE <leeh1102@gmail.com>
2022-02-02 23:49:30 -0800

    Add functions to budget.py

--- budget.py
+++ budget.py
@@ -1,5 +1,6 @@
 from datetime import datetime
-import transaction
+from transaction import Transaction
+
 
 
 class Budget:
@@ -18,9 +19,9 @@ class Budget:
         """
         self._name = name
         self._budget_limit = budget_limit
-        self._expenses = expenses
+        self._expenses = 0
         self._remaining_balance = remaining_balance
-        self._transaction_list = transaction_list
+        self._transaction_list = []
         self._is_locked = is_locked
 
     # Accessors
@@ -40,10 +41,22 @@ class Budget:
 
     # remaining_balance = property(get_remaining_balance)
 
-    def create_transaction(self, timestamp: datetime, dollar_amount: float, expense_location: str) -> None:
+    def create_transaction(self, timestamp: datetime, dollar_amount: float, expense_location: str) -> Transaction:
         """
         Create a new transaction.
         :param timestamp: a datetime representing the time of the transaction
         :param dollar_amount: a float describing amount in dollar for the transaction
         :param expense_location: a string describing the location of expense
         """
+        new_transaction = Transaction(timestamp, dollar_amount, expense_location)
+        self._add_to_transaction(new_transaction)
+        return new_transaction
+
+    def _add_to_transaction(self, transaction: Transaction) -> None:
+        """
+        Add a transaction to the list.
+        :param transaction: Transaction
+        """
+        self._transaction_list.apeend(transaction)
+
+
