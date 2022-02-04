c035f5e7eaf9780a66083dd03046c7a3da872368
HEATHERG LEE <leeh1102@gmail.com>
2022-02-04 15:49:39 -0800

    Add functions to budget.py

--- budget.py
+++ budget.py
@@ -2,7 +2,6 @@ from datetime import datetime
 from transaction import Transaction
 
 
-
 class Budget:
     """ A Budget Class"""
 
@@ -39,6 +38,10 @@ class Budget:
         """
         return self._expenses
 
+    # Properties
+    remaining_balance = property(get_remaining_balance)
+    expenses = property(get_expenses)
+
     # remaining_balance = property(get_remaining_balance)
 
     def create_transaction(self, timestamp: datetime, dollar_amount: float, expense_location: str) -> Transaction:
@@ -58,5 +61,90 @@ class Budget:
         :param transaction: Transaction
         """
         self._transaction_list.apeend(transaction)
+
+    def get_budgets(self):
+        """
+        Return the current status of user's budgets from all categories.
+        :return:
+        """
+        # TODO: More logic required on determining locked account.
+        if self._is_locked:
+            return True
+        # TODO: math needed to added for each category
+        print("--- Current account status --- "
+              "Amount spent: ", self.remaining_balance,
+              "Remaining amount: \n"
+              "1. Games and Entertainment: ",
+              "2. Clothing and Accessories: ",
+              "3. Eating out: " ,
+              "4. Miscellaneous: ",)
+
+
+
+    def prompt_record_transaction(self) -> None:
+        """
+        Prompt inputs to user asking for details of a new transaction.
+        """
+        string_input = input("Please enter the following details to create a transaction.")
+        user_input = int(string_input)
+        try:
+            if user_input == '':
+                print("Please enter valid number between 1 - 4.")
+                pass
+        except ValueError:
+            print("Invalid input. Please enter valid number between 1 -4.")
+        else:
+            # TODO: Is the timestamp going to be current time?
+            input_timestamp = datetime.now()
+            input_budget_category = int(input("Please enter the category: \n"
+                                              "1. Games and Entertainment\n"
+                                              "2. Clothing and Accessories\n"
+                                              "3. Eating out\n"
+                                              "4. Miscellaneous\n"))
+            input_dollar_amount = int(input("Please enter the amount: "))
+
+            input_expense_location = input("Please enter the name of "
+                                           "the shop/website where the purchase took place: ")
+
+            new_transaction = Transaction(input_timestamp, input_budget_category,
+                                          input_dollar_amount, input_expense_location)
+
+
+    def transactions_by_budget(self) -> list:
+        """
+        Return a list of transactions by budget category selected by an user.
+        :return: a list of transactions
+        """
+        string_input = input("Please enter one budget category you would like to see: \n"
+                             "1. Games and Entertainment\n"
+                             "2. Clothing and Accessories\n"
+                             "3. Eating out\n"
+                             "4. Miscellaneous\n")
+        user_input = int(string_input)
+        try:
+            # TODO: add logics to retrieve transactions.
+            if user_input == 1:
+                print("*** The list of transaction in Games and Entertainment ***\n"
+                      "")
+
+            elif user_input == 2:
+                print("*** The list of transaction in Clothing and Accessories ***\n"
+                      "")
+
+            elif user_input == 3:
+                print("*** The list of transaction in Eating out ***\n"
+                      "")
+
+            elif user_input == 4:
+                print("*** The list of transaction in Miscellaneous ***\n"
+                      "")
+
+        except ValueError:
+            print("Invalid input. Please enter valid number between 1 -4.")
+
+        else:
+            print("Please enter valid number between 1 - 4.")
+            pass
+
 
 
